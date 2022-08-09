43293f0c2e3aaef792821bd2c259e2ea92364b76
Abhay Tomar <77535285+abhaytomar2002@users.noreply.github.com>
2022-08-09 16:24:52 +0530

    Update check-auth.js

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,11 +1,13 @@
+
+//Requiring jsonwebtoken for authentication
 const jwt = require("jsonwebtoken");
 
 
 module.exports = (req, res, next) =>{
     try{
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];//token from header section of postman
         console.log(token);
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_KEY);//token verification
         req.userData = decoded;
         next();
     }catch(error){
