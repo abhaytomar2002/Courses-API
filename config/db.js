db7257e1d0961c1a3cb3aa10f6eac3ad8100d25b
Abhay Tomar <77535285+abhaytomar2002@users.noreply.github.com>
2022-08-24 15:34:42 +0530

    Security commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
 
-    return mongoose.connect("mongodb+srv://abhaytomar2002:yDXDS15mg58zBTvG@cluster0.rvkou.mongodb.net/courseDB?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://<username>:<password>@cluster0.rvkou.mongodb.net/courseDB?retryWrites=true&w=majority")
 };
 
 module.exports = connect;
